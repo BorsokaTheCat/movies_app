01c7982f41fd5838230133d8bef65b9bc3afe885
Katona Eszter <keszter1998@gmail.com>
2024-06-01 13:28:20 +0200

    add ui modifications to the movie details page

--- lib/screens/movie_details_screen.dart
+++ lib/screens/movie_details_screen.dart
@@ -11,25 +11,72 @@ class MovieDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(movie.title),
-      ),
+      appBar: null, // Hide the app bar
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.network(
-            'https://image.tmdb.org/t/p/w200${movie.posterPath}',
-            fit: BoxFit.cover,
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height/2,
+                width: MediaQuery.of(context).size.width,
+                child: Image.network(
+                  'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).size.height/2 - 100,
+                left: 20,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      movie.title,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Icon(Icons.star, color: Colors.yellow, size: 16),
+                        SizedBox(width: 6),
+                        Text(
+                          movie.voteAverage.toString(),
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.yellow,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(20.0),
             child: Text(
               movie.overview,
-              style: TextStyle(fontSize: 16),
+              style: TextStyle(fontSize: 16,
+              color: Color(0xFF969696)),
             ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Icon(Icons.arrow_back, color: Colors.white), // Custom back button
+        backgroundColor: Colors.transparent, // Background color of the button
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
     );
   }
 }
